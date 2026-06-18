46b5717275fa6e99954363101d5bd6aa9d489229
HarshaRathore231 <109207537+HarshaRathore231@users.noreply.github.com>
2026-06-18 01:36:37 -0700

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
-        return "Hello Harsha DevOps Project!";
+        return "Hello DevOps Project!";
     }
 
     public static void main(String[] args) {
